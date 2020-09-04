44cf68ea334af11014f35d70dc5d6744b2ee14bd
Katheryn Martinez Hernandez <kaymartinezh@cs.stonybrook.edu>
2020-09-04 01:52:18 -0400

    implement audio_write_header support; clean code

--- hw1/src/audio.c
+++ hw1/src/audio.c
@@ -30,42 +30,79 @@ int audio_read_header(FILE *in, AUDIO_HEADER *hp) {
     int m_number;
     if(read_bytes(in, &m_number) < 0) {
         return EOF;
-    } else hp -> magic_number = m_number;
+    }
+    hp -> magic_number = m_number;
     //get data offset
     int d_offset;
     if(read_bytes(in, &d_offset) < 0) {
         return EOF;
-    } else hp -> data_offset = d_offset;
+    }
+    hp -> data_offset = d_offset;
     //get data size
     int d_size;
     if(read_bytes(in, &d_size) < 0) {
         return EOF;
-    } else hp -> data_size = d_size;
+    }
+    hp -> data_size = d_size;
     //get encoding
     int encode;
     if(read_bytes(in, &encode) < 0) {
         return EOF;
-    } else hp -> encoding = encode;
+    }
+    hp -> encoding = encode;
     //get sample rate
     int s_rate;
     if(read_bytes(in, &s_rate) < 0) {
         return EOF;
-    } else hp -> sample_rate = s_rate;
+    }
+    hp -> sample_rate = s_rate;
     //get channels
     int chan;
     if(read_bytes(in, &chan) < 0) {
 
-    } else hp -> channels = chan;
+    }
+    hp -> channels = chan;
     //check if the header is valid
     if(m_number == AUDIO_MAGIC && encode == PCM16_ENCODING && chan == AUDIO_CHANNELS) {
+        //move pointer to start of data (offset - header = annotation)
+        //current pointer is at end of header, now move to after annotations end (which is start of data)
+        int annotations = d_offset - 24;
+        for(int i = 0; i < annotations; i++) {
+            fgetc(in);
+        }
         return 0;
-        debug("SAME MAGIC");
     } else return EOF;
+}
+
+int write_bytes(FILE *out, int field) {
+    int current;
+    int m_number = field;
+    for(int i = 24; i >= 0;) {
+        current = m_number >> i;
+        int output = fputc(current, out);
+        //debug("%d",output);
+        if(output == EOF)
+            return -1;
+        i = i- 8;
+    }
+    return 0;
 }
 
 int audio_write_header(FILE *out, AUDIO_HEADER *hp) {
     // TO BE IMPLEMENTED
-    return EOF;
+    if(write_bytes(out, hp -> magic_number) < 0)
+        return EOF;
+    if(write_bytes(out, hp -> data_offset) < 0)
+        return EOF;
+    if(write_bytes(out, hp -> data_size) < 0)
+        return EOF;
+    if(write_bytes(out, hp -> encoding) < 0)
+        return EOF;
+    if(write_bytes(out, hp -> sample_rate) < 0)
+        return EOF;
+    if(write_bytes(out, hp -> channels) < 0)
+        return EOF;
+    return 0;
 }
 
 int audio_read_sample(FILE *in, int16_t *samplep) {
