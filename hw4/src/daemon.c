9f97b32854503a1e8b3a188d0cd01443dcf22c4a
Katheryn Martinez Hernandez <kaymartinezh@cs.stonybrook.edu>
2020-11-06 23:28:14 -0500

    Implement data structure functions

--- hw4/src/daemon.c
+++ hw4/src/daemon.c
@@ -15,11 +15,42 @@ void add_daemon(D_STRUCT *daemon) {
 }
 
 /* get daemon from list */
-void get_daemon(char *daemon);
+D_STRUCT *get_daemon(char *d_name) {
+	while(head != NULL) {
+		if(head->name == d_name)
+			return head;
+		head = head->next;
+	}
+	return NULL;
+}
 
 /* remove daemon from list note: might need two vars */
-void remove_daemon(D_STRUCT *daemon);
+void remove_daemon(char *d_name) {
+	D_STRUCT *temp = head;
+	D_STRUCT *prev = NULL;
+	/* if current head contains d_name */
+	if(temp != NULL && temp->name == d_name) {
+		/* remove current head and set head's next as new head */
+		head = temp->next;
+		return;
+	}
+	/* while daemon is not found, keep traversing (while setting prev daemon) */
+	while(temp != NULL && temp->name != d_name) {
+		/* set previous to current */
+		prev = temp;
+		/* and set current to next */
+		temp = temp->next;
+	}
+	if(temp == NULL) return;
+	/* unlink/remove daemon */
+	prev->next = temp->next;
+}
 
 /* iterate through daemons */
-void print_daemon();
+void print_daemon() {
+	while(head != NULL) {
+		printf("Daemon [Name %s, Command %s, Status %d\n]", head->name, head->command, head->status);
+		head = head->next;
+	}
+}
 
