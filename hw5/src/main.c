a39f10c4717e8274656856cb106a9c4863b6e93d
Katheryn Martinez Hernandez <kaymartinezh@cs.stonybrook.edu>
2020-11-21 23:42:32 -0500

    Implement server initialization (task I)

--- hw5/src/main.c
+++ hw5/src/main.c
@@ -15,12 +15,16 @@
 #include "client_registry.h"
 #include "player_registry.h"
 #include "jeux_globals.h"
+#include "csapp.h"
 
 #ifdef DEBUG
 int _debug_packets_ = 1;
 #endif
 
 static void terminate(int status);
+static void sighup_handler(int sig) {
+    terminate(0);
+}
 
 /*
  * "Jeux" game server.
@@ -31,6 +35,22 @@ int main(int argc, char* argv[]){
     // Option processing should be performed here.
     // Option '-p <port>' is required in order to specify the port number
     // on which the server should listen.
+    char *port_num;
+    int listen_fd, *conn_fd;
+    socklen_t client_len;
+    struct sockaddr_storage client_addr;
+    pthread_t tid;
+
+    if(argc != 3) {
+        fprintf(stderr, "Usage: demo/jeux -p <port>\n");
+        exit(1);
+    }
+    if(strcmp(argv[1], "-p") == 0) {
+        port_num = argv[2];
+    } else {
+        fprintf(stderr, "Usage: demo/jeux -p <port>\n");
+        exit(1);
+    }
 
     // Perform required initializations of the client_registry and
     // player_registry.
@@ -42,11 +62,39 @@ int main(int argc, char* argv[]){
     // run function jeux_client_service().  In addition, you should install
     // a SIGHUP handler, so that receipt of SIGHUP will perform a clean
     // shutdown of the server.
+    struct sigaction s_action;
+    s_action.sa_handler = sighup_handler;
+    sigemptyset(&s_action.sa_mask);
+    s_action.sa_flags = 0;
+    if (sigaction(SIGHUP, &s_action, NULL) == -1) {
+        fprintf(stderr, "SIGHUP error\n");
+        exit(1);
+    }
+    if((listen_fd = open_listenfd(port_num)) < 0) {
+        fprintf(stderr, "open_listenfd error\n");
+        exit(1);
+    }
+    debug("Jeux server listening on port %s", port_num);
+    while(1) {
+        client_len = sizeof(struct sockaddr_storage);
+        conn_fd = malloc(sizeof(int));
+        if(conn_fd == NULL) {
+            fprintf(stderr, "conn_fd malloc error\n");
+            exit(1);
+        }
+        if((*conn_fd = accept(listen_fd, (SA *)&client_addr, &client_len)) < 0) {
+            fprintf(stderr, "conn_fd accept error\n");
+            exit(1);
+        }
+        Pthread_create(&tid, NULL, jeux_client_service, conn_fd);
+    }
 
-    fprintf(stderr, "You have to finish implementing main() "
+    /*fprintf(stderr, "You have to finish implementing main() "
 	    "before the Jeux server will function.\n");
 
-    terminate(EXIT_FAILURE);
+    terminate(EXIT_FAILURE);*/
+
+    return 0;
 }
 
 /*
@@ -56,7 +104,7 @@ void terminate(int status) {
     // Shutdown all client connections.
     // This will trigger the eventual termination of service threads.
     creg_shutdown_all(client_registry);
-    
+
     debug("%ld: Waiting for service threads to terminate...", pthread_self());
     creg_wait_for_empty(client_registry);
     debug("%ld: All service threads terminated.", pthread_self());
