7944b8e754c06dae7aa2d05d7253b90e745abafc
Katheryn Martinez Hernandez <kaymartinezh@cs.stonybrook.edu>
2020-11-24 01:14:58 -0500

    Fix SIGHUP handling; add comments

--- hw5/src/main.c
+++ hw5/src/main.c
@@ -37,21 +37,26 @@ int main(int argc, char* argv[]){
     // Option processing should be performed here.
     // Option '-p <port>' is required in order to specify the port number
     // on which the server should listen.
+
+    /* vars for starting up server */
     char *port_num;
     int listen_fd, *conn_fd;
     socklen_t client_len;
     struct sockaddr_storage client_addr;
     pthread_t tid;
+    //sighup_flag = 0;
 
+    /* check that the given args is the right number */
     if(argc != 3) {
         fprintf(stderr, "Usage: demo/jeux -p <port>\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
+    /* if so, check for port number */
     if(strcmp(argv[1], "-p") == 0) {
         port_num = argv[2];
     } else {
         fprintf(stderr, "Usage: demo/jeux -p <port>\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     // Perform required initializations of the client_registry and
@@ -64,17 +69,20 @@ int main(int argc, char* argv[]){
     // run function jeux_client_service().  In addition, you should install
     // a SIGHUP handler, so that receipt of SIGHUP will perform a clean
     // shutdown of the server.
+
+    /* install sigaction for SIGHUP */
     struct sigaction s_action;
     s_action.sa_handler = sighup_handler;
     sigemptyset(&s_action.sa_mask);
     s_action.sa_flags = 0;
     if (sigaction(SIGHUP, &s_action, NULL) == -1) {
         fprintf(stderr, "SIGHUP error\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
+    /* attempt to start server */
     if((listen_fd = open_listenfd(port_num)) < 0) {
         fprintf(stderr, "open_listenfd error\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     debug("Jeux server listening on port %s", port_num);
     while(1) {
@@ -82,22 +90,18 @@ int main(int argc, char* argv[]){
         conn_fd = malloc(sizeof(int));
         if(conn_fd == NULL) {
             fprintf(stderr, "conn_fd malloc error\n");
-            exit(1);
+            exit(EXIT_FAILURE);
         }
         if((*conn_fd = accept(listen_fd, (SA *)&client_addr, &client_len)) < 0) {
+            /* check for SIGHUP */
+            if(sighup_flag) terminate(EXIT_SUCCESS);
+            /* if not sIGHUP, something else went wrong */
             fprintf(stderr, "conn_fd accept error\n");
-            exit(1);
+            exit(EXIT_FAILURE);
         }
         Pthread_create(&tid, NULL, jeux_client_service, conn_fd);
-        if(sighup_flag) terminate(0);
     }
-
-    /*fprintf(stderr, "You have to finish implementing main() "
-	    "before the Jeux server will function.\n");
-
-    terminate(EXIT_FAILURE);*/
-
-    return 0;
+    exit(EXIT_SUCCESS);
 }
 
 /*
