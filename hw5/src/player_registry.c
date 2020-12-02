d485590a2e1b8a29866756fd72d3249e4fc16783
Katheryn Martinez Hernandez <kaymartinezh@cs.stonybrook.edu>
2020-12-02 13:15:15 -0500

    Add free registry functionality; fix ref counts

--- hw5/src/player_registry.c
+++ hw5/src/player_registry.c
@@ -37,11 +37,29 @@ PLAYER_REGISTRY *preg_init(void) {
 
 void preg_fini(PLAYER_REGISTRY *preg) {
 	debug("%lu: Finalize player registry", pthread_self());
+	/* lock mutex */
+	if(pthread_mutex_lock(&preg->mutex) != 0) {
+		debug("pthread_mutex_lock error");
+	}
+	/* free player registry */
+	struct p_list *temp;
+	struct p_list *root = preg->list;
+	while(root != NULL) {
+		temp = root;
+		root = root->next;
+		temp->prev = NULL;
+		temp->next = NULL;
+		player_unref(temp->player, "because player registry is being finalized");
+		free(temp);
+	}
+	/* lock mutex */
+	if(pthread_mutex_unlock(&preg->mutex) != 0) {
+		debug("pthread_mutex_unlock error");
+	}
 	/* destroy mutex */
 	if(pthread_mutex_destroy(&preg->mutex) != 0) {
 		debug("error destroying mutex");
 	}
-	/* free player registry */
 	free(preg);
 }
 
@@ -52,7 +70,7 @@ PLAYER *preg_register(PLAYER_REGISTRY *preg, char *name) {
 		debug("pthread_mutex_lock error");
 		return NULL;
 	}
-	/* copy name */
+	/* copy name
 	char *new_name = strdup(name);
 	if(new_name == NULL) {
 		debug("error copying player name");
@@ -61,21 +79,22 @@ PLAYER *preg_register(PLAYER_REGISTRY *preg, char *name) {
 			return NULL;
 		}
 		return NULL;
-	}
-	//debug("PLAYER NEW NAME %s", new_name);
+	} */
+	debug("Register player %s", name);
 	struct p_list **root = &preg->list;
 	struct p_list *temp_prev = NULL;
 	/* search through each player registered first */
 	while(*root != NULL) {
 		char *p_name = player_get_name((*root)->player);
 		/* check if a player with that username exists */
-		if(strcmp(p_name, new_name) == 0) {
+		if(strcmp(p_name, name) == 0) {
 			p = (*root)->player;
 			/* if so, increment player ref count by one */
-			player_ref(p, "for reference being returned by preg_register()");
+			player_ref(p, "for reference being retained by player registry");
 			/* and return existing player */
 			if(pthread_mutex_unlock(&preg->mutex) != 0) {
 				debug("pthread_mutex_unlock error");
+				player_unref(p, "for reference being released by player registry");
 				return NULL;
 			}
 			return p;
@@ -83,6 +102,7 @@ PLAYER *preg_register(PLAYER_REGISTRY *preg, char *name) {
 		temp_prev = (*root);
 		root = &((*root)->next);
 	}
+	debug("Player with that name does not yet exist");
 	/* allocate space for players list */
 	*root = malloc(sizeof(struct p_list));
 	if(*root == NULL) {
@@ -95,11 +115,14 @@ PLAYER *preg_register(PLAYER_REGISTRY *preg, char *name) {
 
 	(*root)->next = NULL;
 	(*root)->prev = temp_prev;
-	(*root)->player = player_create(new_name);
+	if(temp_prev != NULL) {
+		temp_prev->next = *root;
+	}
+	(*root)->player = player_create(name);
 	if((*root)->player != NULL) {
 		p = (*root)->player;
 		/* increment player ref count by one */
-		player_ref(p, "for reference being created by preg_register()");
+		player_ref(p, "for reference being retained by player registry");
 	}
 	/* increment player count by one */
 	preg->num_players++;
